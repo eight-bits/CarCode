22dc574c915d12b793f7041f5152ed7d985011d1
Andrey Kudryavtsev <eightkbit@gmail.com>
2020-12-15 13:08:48 +0500

    Initial Commit

---
+++ CarCode/CarCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCodeApp.swift
+//  CarCode
+//
+//  Created by Andrey Kudryavtsev on 15.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct CarCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
